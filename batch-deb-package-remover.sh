f4bc4ca3546a5626bdd5ad333f17a7476c865e67
Zoran Olujic <olujicz@gmail.com>
2012-12-05 10:27:08 +0100

    Added $1 as option

--- batch-deb-package-remover.sh
+++ batch-deb-package-remover.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 # This script remove all installed packages wich contains word you enter
-echo "Type the word wich you want to remove all package containing it, followed by [ENTER]:"
-read package_name
-if [ -z "$package_name" ];
+
+if [ -z "$1" ];
     then
-        echo "You need to type word"
+        echo "Type the word wich you want to remove all package containing it, followed by [ENTER]:"
+        read package_name
+            if [ -z "$package_name" ]; then
+                echo "You need to type word"
+                exit 1
+            else
+                sudo apt-get purge $(dpkg --get-selections | grep $package_name | sed s/'\w*$'//)
+            fi
     else
-        sudo apt-get purge $(dpkg --get-selections | grep $package_name | sed s/'\w*$'//)
+                sudo apt-get purge $(dpkg --get-selections | grep $1 | sed s/'\w*$'//)
 fi
+
 exit 0
