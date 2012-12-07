6521e938add6ce5331d20bcd1488eba03fd86a45
Zoran Olujic <oluja@localhost.localdomain>
2012-12-07 09:10:20 +0100

    small fix

--- mysql-dump.sh
+++ mysql-dump.sh
@@ -21,7 +21,7 @@ password="emos"
 exec 3>&1 4>&2
 trap 'exec 2>&4 1>&3' 0 1 2 3
 exec 1>>$backup_folder/$database_name.log 2>&1
-# Everything below will go to the file 'log.out':
+# Everything below will go to the log file:
 
 
 dump_base () {
