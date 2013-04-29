f175189e7ef9eeabea9f67eec8b0e360cdf47d18
Zoran Olujic <olujicz@olujicz-pc.(none)>
2013-04-29 10:46:42 +0200

    Script to install fish in Ubuntu

---
+++ ubuntu/install-fish.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Install fish
+sudo apt-get -y install python-software-properties
+audo apt-get -y install software-properties-common
+sudo add-apt-repository ppa:zanchey/fishfish-snapshot
+sudo aptitude update
+sudo aptitude -y install fishfish
+
+# Install git
+sudo apt-get install git-core
+
+# configuration
+mkdir .config
+cd ~/.config
+git clone https://github.com/mekanix/fish.git 
+
+# change shell
+chsh -s /usr/bin/fish
+
